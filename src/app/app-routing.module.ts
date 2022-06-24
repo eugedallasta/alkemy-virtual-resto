69245bd1a6ee425f9f794ef56ef245cd3f13ff00
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-06-24 19:24:07 -0300

    add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { DishSearchComponent } from './components/dish-search/dish-search.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { LoginGuardGuard } from './guards/login-guard.guard';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'search-dish', component: DishSearchComponent },
+  { path: 'home', component: HomeComponent, canActivate:[LoginGuardGuard]},
+  { path: 'search-dish', component: DishSearchComponent, canActivate:[LoginGuardGuard] },
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
--- src/app/guards/login-guard.guard.ts
+++ src/app/guards/login-guard.guard.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuardGuard implements CanActivate {
+  constructor(private router :Router) {}
+
+  //Redirecciona al login si recibe false
+  redirect(bool :boolean) {
+    if(bool) {
+      this.router.navigate(["login"]);
+    }
+  }
+  //Si no se encuentra un token en el local storage redirecciona al login y retorna false
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+    ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const token = sessionStorage.getItem("token") === null;
+    this.redirect(token);
+    return !token;
   }
-  
+
 }
